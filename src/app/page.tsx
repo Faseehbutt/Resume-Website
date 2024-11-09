1c3aeede25b4fef53c69ae69b2b5670d5f16b0f7
Faseehbutt <notfaseehbutt@gmail.com>
2024-11-10 01:07:16 +0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+import Image from "next/image"
+import Link from "next/link"
+export default function Homepage(){
+  return (
+    <div>
+    <div className="body">
+      <Image className="prgrmr-img"
+                src="/programmer.png"
+                alt="Description of image"
+                width={500}
+                height={500}
+                quality={100}
+            />
+      <div className="bodytext">
+        
+    <h1><span>Hi,</span> My name is Mohammad Faseeh,</h1>
+    <p>I am a resume builder</p>
+    <a href="/resume.pdf" download="My Resume"> 
+    <button>Download Now</button>
+    </a>
+    </div>
+</div>
+</div>
+  )
+}

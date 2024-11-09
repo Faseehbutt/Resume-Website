a39130ae0b05499cf96c11e7fd6f33576d679f37
Faseehbutt <notfaseehbutt@gmail.com>
2024-11-10 02:08:16 +0500

    file delete

--- src/app/About-Me/page.tsx
+++ src/app/About-Me/page.tsx
@@ -4,8 +4,7 @@ export default function About(){
         <div className="about-me">
    <h2>About Me</h2>
     <p>Hello! I&apos;m <span>Mohammad Faseeh</span>, a passionate resume builder and student originally from Sukkur. My journey has currently brought me to Karachi, where I&apos;m deeply engaged in a specialized course under the <span>Governor Sindh Initiative</span>. This experience is helping me expand my knowledge in <span>GenAI, Web3,</span> and the <span>Metaverse</span>.</p>
-    <p>In addition to pursuing my <span>FSc from Govt Islamia Science College Sukkur</span>, I'm dedicated to refining my skills in web development, working on projects that allow me to grow and help others present their unique talents through well-crafted resumes.</p>
+    <p>In addition to pursuing my <span>FSc from Govt Islamia Science College Sukkur</span>, I&apos;m dedicated to refining my skills in web development, working on projects that allow me to grow and help others present their unique talents through well-crafted resumes.</p>
 </div>
-
     )
 }

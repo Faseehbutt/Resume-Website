2bf8395c8d58b94e2a8747e4bbf3f7dceb971b29
Faseehbutt <160475773+Faseehbutt@users.noreply.github.com>
2025-01-03 01:24:45 +0500

    Update index.ts

--- src/app/resume-generator/types/index.ts
+++ src/app/resume-generator/types/index.ts
@@ -1,3 +1,24 @@
+export interface BasicInfo {
+  name: string;
+  email: string;
+  phone: string;
+  objective: string;
+  image?: string;
+}
+export interface Education {
+  id: string;
+  school: string;
+  degree: string;
+  year: string;
+}
+export interface Experience {
+  id: string;
+  company: string;
+  position: string;
+  duration: string;
+  description: string;
+}
+
 export interface ResumeData {
   basic: {
     name: string;
